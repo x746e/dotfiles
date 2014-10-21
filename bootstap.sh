a792480f84079e49e5a1f9876a0984b648b94aa0
U-THEFACEBOOK\kyrylo <tn@0x746e.org.ua>
2014-10-21 11:52:30 -0700

    Modified bootstrapping script a bit.

--- bootstap.sh
+++ bootstap.sh
@@ -6,7 +6,7 @@
 cd "$(dirname "$0")/.."
 DOTFILES_ROOT=$(pwd)
 
-set -e
+set -e -u
 
 echo ''
 
@@ -27,29 +27,6 @@ fail () {
   echo ''
   exit
 }
-
-setup_gitconfig () {
-  if ! [ -f git/gitconfig.symlink ]
-  then
-    info 'setup gitconfig'
-
-    git_credential='cache'
-    if [ "$(uname -s)" == "Darwin" ]
-    then
-      git_credential='osxkeychain'
-    fi
-
-    user ' - What is your github author name?'
-    read -e git_authorname
-    user ' - What is your github author email?'
-    read -e git_authoremail
-
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.symlink.example > git/gitconfig.symlink
-
-    success 'gitconfig'
-  fi
-}
-
 
 link_file () {
   local src=$1 dst=$2
@@ -137,20 +114,12 @@ install_dotfiles () {
   done
 }
 
-setup_gitconfig
-install_dotfiles
+run_installers () {
+  find "$DOTFILES_ROOT" -maxdepth 2 -name install.sh | while read installer ; do sh -c "${installer}" ; done
+}
 
-# If we're on a Mac, let's install and setup homebrew.
-if [ "$(uname -s)" == "Darwin" ]
-then
-  info "installing dependencies"
-  if source bin/dot > /tmp/dotfiles-dot 2>&1
-  then
-    success "dependencies installed"
-  else
-    fail "error installing dependencies"
-  fi
-fi
+install_dotfiles
+run_installers
 
 echo ''
 echo '  All installed!'
