f6663593cb25fb25d07877a607a7d244d182e945
Kirill Spitsyn <kyrylo@fb.com>
2014-10-21 13:57:52 -0700

    Improved vim's install script.

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,6 @@
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+BUNDLE_DIR="~/.vim/bundle"
+if [ ! -d "$BUNDLE_DIR" ]; then
+    mkdir "$BUNDLE_DIR"
+    git clone https://github.com/gmarik/Vundle.vim.git "$BUNDLE_DIR/Vundle.vim"
+fi
 vim +PluginInstall +qall
