64a1b65bfdd65c551e1aa02ce8defa1608bffe39
Kirill Spitsyn <ksp@google.com>
2016-08-17 10:20:30 -0700

    [git] Scripts for working with a stacked branches.
    git amend -- used for changing a branch that has child branches.
    Similar to rebase -i, but also moves all branches to amended child
    commits.
    
    git amend-fixup -- used for fixing up child branches after current
    branch is amended.  Used by `git amend`, and should be run by hand if
    rebase run by `git amend` is interrupted by merge conflict.

---
+++ git/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+set -o errexit
+set -o nounset
+
+DOTFILES_ROOT="$(dirname "$(dirname "$(realpath $0)")")"
+
+source $DOTFILES_ROOT/scripts/lib.sh
+
+for link_target in $(find "$DOTFILES_ROOT/git/scripts" -maxdepth 1 -type f); do
+  link_file="$HOME/bin/$(basename "$link_target")"
+  create_symlink "$link_target" "$link_file"
+done
