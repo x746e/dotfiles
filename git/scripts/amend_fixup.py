fd9aa4870d83766c84b9dfec770887939d07cd79
Kirill Spitsyn <ksp@google.com>
2016-12-07 15:16:36 -0800

    [git stripts] Implemented amend_fixup in python.

---
+++ git/scripts/amend_fixup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# vim: ts=4:sts=4:sw=4
+from subprocess import check_call, check_output, CalledProcessError
+
+
+def main():
+    branches = get_branches()
+    # Get all preamend branches
+    preamend_branches = [branch for branch in branches if branch.startswith('preamend-')]
+
+    for preamend_branch in preamend_branches:
+        amended_branch = preamend_branch[len('preamend-'):]
+        rebase_children(amended_branch, preamend_branch)
+        drop_branch(preamend_branch)
+
+
+def get_branches():
+    return [line.decode('utf-8') for line in
+            check_output("git branch | sed -e 's/^[* ]*//'", shell=True).splitlines()]
+
+
+def drop_branch(branch):
+    run("git branch -D '%s'" % branch)
+
+
+def rebase_children(amended_branch, preamend_branch):
+    branches = get_branches()
+    # Get all direct child branches of preamend branch (not grandchildren).
+    descedants = [
+        branch for branch in branches
+        if is_descedant(branch, preamend_branch)
+    ]
+    children = [
+        branch for branch in descedants
+        if not any([is_descedant(branch, another) for another in descedants])
+            and not branch.startswith('preamend-')
+    ]
+
+    # Rebase children.
+    for child in children:
+        preamend_child = 'preamend-%s' % child
+        try:
+            rebase(onto=amended_branch, upstream=preamend_branch, branch=child)
+        except CalledProcessError:
+            run("git st")
+            print("Seems like a conflict during rebase.  Fix the conflict, run "
+                  "git add .; git rebase --continue, and then git amend-fixup again")
+            exit(1)
+        rebase_children(amended_branch=child, preamend_branch=preamend_child)
+        # Cleanup successfully rebased children.
+        preamend_child_has_children = [branch for branch in get_branches()
+                                       if is_descedant(branch, preamend_child)]
+        assert not preamend_child_has_children
+        drop_branch(preamend_child)
+
+
+def is_descedant(branch, ancestor):
+    """Whether `ancestor` in an ancestor of `branch`."""
+    if branch == ancestor:
+        return False
+    descedants = [
+        line.decode('utf-8') for line in
+        check_output("git branch --contains '%s' | sed -e 's/^[* ]*//'" % ancestor, shell=True).splitlines()
+    ]
+    return branch in descedants
+
+
+def run(cmd):
+    print(">> executing: %s" % cmd)
+    check_call(cmd, shell=True)
+
+
+def rebase(onto, upstream, branch):
+    run("git branch 'preamend-{branch}' '{branch}'".format(branch=branch))
+    run("git rebase --onto '%s' '%s' '%s'" % (onto, upstream, branch))
---
+++ git/scripts/test_amend_fixup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# vim: ts=4:sts=4:sw=4
+from unittest import TestCase, main
+
+
+class AmendFixupTest(TestCase):
+
+    def test_get_branches(self):
+        assert False
+
+
+if __name__ == '__main__':
+    main()
