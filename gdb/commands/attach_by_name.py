82de19af637f78732375e68932199b327a850d5d
Kirill Spitsyn <ksp@google.com>
2019-10-02 15:01:12 -0700

    [gdb] Add `attach-by-name` command.

---
+++ gdb/commands/attach_by_name.py
@@ -0,0 +1,19 @@
+import subprocess
+
+class AttachByName(gdb.Command):
+    def __init__(self):
+        super(AttachByName, self).__init__('attach-by-name', gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        try:
+            out = subprocess.check_output('pgrep -x %s' % arg, shell=True).strip()
+        except subprocess.CalledProcessError:
+            raise gdb.GdbError("No process called '%s' was found.")
+        assert out, 'empty output'
+        pids = map(int, out.split())
+        if len(pids) > 1:
+            raise gdb.GdbError("There are more than one process called '%s'." % arg)
+        (pid,) = pids
+        gdb.execute('attach %d' % pid, from_tty=from_ttyTrue)
+
+AttachByName()
