14aaaf656e620a7a1c252cfbd5986dc32aa56e43
Kirill Spitsyn <ksp@google.com>
2020-06-09 09:40:14 -0700

    [git] mkdir ~/bin to have where to put custom git commands.

--- git/install.sh
+++ git/install.sh
@@ -7,6 +7,8 @@ DOTFILES_ROOT="$(dirname "$(dirname "$(realpath $0)")")"
 
 source $DOTFILES_ROOT/scripts/lib.zsh
 
+mkdir "$HOME/bin"
+
 for link_target in $(find "$DOTFILES_ROOT/git/scripts" -maxdepth 1 -name 'git-*' -type f); do
   link_file="$HOME/bin/$(basename "$link_target")"
   create_symlink "$link_target" "$link_file"
