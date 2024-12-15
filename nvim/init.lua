46d0e6528f525aa5092adb5c987a6519e8bd1809
Kirill Spitsyn <ksp@google.com>
2024-12-14 20:20:03 -0800

    [nvim] Install nvim-treesitter
    I need it now for syntax highlighting in the Telescope's buffer explorer, but it probably has other uses as well.

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,8 @@ vim.call('plug#begin')
 -- Why in hell do I need to setup dependencies manually?...
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
 
 vim.call('plug#end')
 
