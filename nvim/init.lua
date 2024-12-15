a1c6ed03a70c0390a45e993482bd00b9c3a832c7
Kirill Spitsyn <ksp@google.com>
2024-12-14 19:24:11 -0800

    Install nvim-telescope with vim-plug.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,15 @@
 vim.o.wildmode = 'list:longest,full'
 vim.cmd.colorscheme('vim')
 vim.g.mapleader = ','
+
+
+-- vim-plug configuration.
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- Why in hell do I need to setup dependencies manually?...
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+
+vim.call('plug#end')
