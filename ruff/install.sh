31b940fe0320aef81cb9774736e5c0180cbb7441
Kirill Spitsyn <k@spitsyn.me>
2026-02-06 14:35:03 -0800

    [ruff] Add configuration and install script

---
+++ ruff/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+set -o errexit
+set -o nounset
+
+DOTFILES_ROOT="$(dirname "$(dirname "$(realpath $0)")")"
+source "$DOTFILES_ROOT/scripts/lib.zsh"
+
+HERE="$(dirname "$(realpath $0)")"
+CONF_DIR="$HOME/.config"
+
+mkdir -p "$CONF_DIR"
+create_symlink "$HERE/config" "$CONF_DIR/ruff"
